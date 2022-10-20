e6f2cea9b93b368398edc391b5cb5e0e8d17ec4d
Jakob Fritz <j.fritz@fz-juelich.de>
2022-10-20 12:00:55 +0200

    Renamed remote to gitlab to avoid confusion with option "mirror"
    Specified gitlab-repo-url via variable

--- bin/mirror.sh
+++ bin/mirror.sh
@@ -3,6 +3,6 @@
 sh -c "git config --global credentials.username $GITLAB_USERNAME"
 sh -c "git config --global core.askPass /getpasswd.sh"
 sh -c "git config --global credential.helper cache"
-sh -c "git remote add mirror $*"
+sh -c "git remote add gitlab $GITLAB_REPO_URL"
 sh -c "echo pushing to $branch branch at $(git remote get-url --push mirror)"
-sh -c "git push --force mirror $branch"
+sh -c "git push --force gitlab $branch"
