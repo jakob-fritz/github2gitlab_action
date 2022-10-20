45cb0f691f49432b38d2033d56dc208b56904f7f
Jakob Fritz <j.fritz@fz-juelich.de>
2022-10-20 11:08:29 +0200

    Moved mirroring to shell-script
    Cloning of Pull-Requests as merge-requests still is done in python

---
+++ bin/mirror.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sh -c "git config --global credentials.username $GITLAB_USERNAME"
+sh -c "git config --global core.askPass /getpasswd.sh"
+sh -c "git config --global credential.helper cache"
+sh -c "git remote add mirror $*"
+sh -c "echo pushing to $branch branch at $(git remote get-url --push mirror)"
+sh -c "git push --force mirror $branch"
