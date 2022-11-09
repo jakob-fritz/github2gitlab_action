0cc9b34c617e6b4790b237a6493c8beb1aefaa3c
Jakob Fritz <j.fritz@fz-juelich.de>
2022-11-09 17:45:11 +0100

    Split into two brackets

--- bin/get_ci_state.sh
+++ bin/get_ci_state.sh
@@ -34,7 +34,7 @@ echo "Poll timeout set to ${POLL_TIMEOUT}"
 
 ci_status="pending"
 
-until [ "$ci_status" != "pending" && "$ci_status" != "running" ]
+until [ "$ci_status" != "pending" ] && [ "$ci_status" != "running" ]
 do
    sleep $POLL_TIMEOUT
    ci_output=$(curl --header "PRIVATE-TOKEN: $GITLAB_PASSWORD" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/${pipeline_id}")
