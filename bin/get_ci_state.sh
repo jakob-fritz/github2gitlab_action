77d8b68022a05657ce7cf70341e36a2bcd289c3e
Jakob Fritz <j.fritz@fz-juelich.de>
2022-11-23 16:02:23 +0100

    Get-CI-State now uses sha to get pipeline-id
    Instead of getting symbolic ref of HEAD

--- bin/get_ci_state.sh
+++ bin/get_ci_state.sh
@@ -21,14 +21,14 @@ urlencode() (
 DEFAULT_POLL_TIMEOUT=10
 POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
-branch="$(git symbolic-ref --short HEAD)"
-branch_uri="$(urlencode ${branch})"
+# branch="$(git symbolic-ref --short HEAD)"
+# branch_uri="$(urlencode ${branch})"
 
 sleep $POLL_TIMEOUT
 
-pipeline_id=$(curl --header "PRIVATE-TOKEN: $GITLAB_PASSWORD" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/commits/${branch_uri}" | jq '.last_pipeline.id')
+pipeline_id=$(curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/commits/${GITHUB_SHA}" | jq '.last_pipeline.id')
 
-echo "Triggered CI for branch ${branch}"
+echo "Triggered CI for branch ${GITHUB_REF}"
 echo "Working with pipeline id #${pipeline_id}"
 echo "Poll timeout set to ${POLL_TIMEOUT}"
 
@@ -37,7 +37,7 @@ ci_status="pending"
 until [ "$ci_status" != "pending" ] && [ "$ci_status" != "running" ]
 do
    sleep $POLL_TIMEOUT
-   ci_output=$(curl --header "PRIVATE-TOKEN: $GITLAB_PASSWORD" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/${pipeline_id}")
+   ci_output=$(curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/${pipeline_id}")
    ci_status=$(jq -n "$ci_output" | jq -r .status)
    ci_web_url=$(jq -n "$ci_output" | jq -r .web_url)
 
@@ -52,11 +52,11 @@ done
 echo "Pipeline finished with status ${ci_status}"
 
 echo "Fetching all GitLab pipeline jobs involved"
-ci_jobs=$(curl --header "PRIVATE-TOKEN: $GITLAB_PASSWORD" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/${pipeline_id}/jobs" | jq -r '.[] | { id, name, stage }')
+ci_jobs=$(curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/${pipeline_id}/jobs" | jq -r '.[] | { id, name, stage }')
 echo "Posting output from all GitLab pipeline jobs"
 for JOB_ID in $(echo $ci_jobs | jq -r .id); do
   echo "##[group]Stage $( echo $ci_jobs | jq -r "select(.id=="$JOB_ID") | .stage" ) / Job $( echo $ci_jobs | jq -r "select(.id=="$JOB_ID") | .name" )"
-  curl --header "PRIVATE-TOKEN: $GITLAB_PASSWORD" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/jobs/${JOB_ID}/trace"
+  curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/jobs/${JOB_ID}/trace"
   echo "##[endgroup]"
 done
 echo "Debug problems by unfolding stages/jobs above"
