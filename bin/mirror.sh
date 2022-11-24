e077503a8ddd21185e988247157fd549d2c45905
Jakob Fritz <j.fritz@fz-juelich.de>
2022-11-24 11:40:52 +0100

    Removed Gitlab-Username and Password

--- bin/mirror.sh
+++ bin/mirror.sh
@@ -8,19 +8,8 @@ POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
 branch="+refs/heads/*:refs/heads/* +refs/tags/*:refs/tags/* +refs/pull/*:refs/heads/pull/*"
 
-# sh -c "pwd"
-# Removing all cached credentials
-# sh -c "git credential-cache exit"
-# sh -c "git config --global --unset-all credential.username"
-# sh -c "git config --global --unset-all core.askPass"
-# sh -c "git config --global credential.helper cache"
-# echo "length of username: "
-# echo -n ${GITLAB_USERNAME} | wc -c
-# echo "length of token: "
-# echo -n ${GITLAB_TOKEN} | wc -c
 echo "adding gitlab-repo as remote"
-git remote add gitlab "https://${GITLAB_USERNAME}:${GITLAB_TOKEN}@${GITLAB_REPO_URL}"
-# sh -c "git remote add gitlab $GITLAB_REPO_URL"
+git remote add gitlab "https://TOKENUSER:${GITLAB_TOKEN}@${GITLAB_REPO_URL}"
 sh -c "git fetch --force origin +refs/heads/*:refs/heads/* +refs/tags/*:refs/tags/* +refs/pull/*:refs/heads/pull/*"
 # sh -c "git fetch origin +refs/pull/*:refs/heads/pull/*"
 # sh -c "echo pushing to $branch branch at $(git remote get-url --push gitlab)"
